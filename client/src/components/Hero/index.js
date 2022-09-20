029b70f5fa721847a95e90d950297085d0239d35
Corinne Mestemacher <corimest@Corinnes-MacBook-Air.local>
2022-09-19 22:11:27 -0500

    changed hero video

--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -1,11 +1,11 @@
 import React from "react";
-import WrappingVideo from '../../assets/wrapping-video.mp4'; 
+import PresentsVideo from '../../assets/present.mp4'; 
 
 function Hero() {
     return (
         <div className="video">
             <div className='overlay'></div>
-            <video autostart autoPlay loop muted src={WrappingVideo} type="wrapping-video/mp4" />
+            <video autostart autoPlay loop muted src={PresentsVideo} type="present/mp4" />
             <div className="hero-text">
                 <h2>Who Are You Shopping For?</h2>
                 <p>Let us be your elves this season with curated gifts to fulfill every Christmas wish</p>
