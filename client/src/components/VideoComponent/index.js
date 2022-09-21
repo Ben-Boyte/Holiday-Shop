782f3d3343e68671cb5287dd5ebe4382810a8110
Corinne Mestemacher <corimest@Corinnes-MacBook-Air.local>
2022-09-20 22:40:16 -0500

    shopping cart text color change, all buttons function, list item style = none, button color hover changed, buttons now in Nav section

--- client/src/components/VideoComponent/index.js
+++ client/src/components/VideoComponent/index.js
@@ -5,7 +5,7 @@ function VideoComponent() {
       return (
        <div className="video-comp">
            <h1 className="hero-title"> Who Are You Shopping For? </h1>
-           <p className="hero-p">Let us be your elves this season with curated gifts to fulfill every Christmas wish</p>
+           <p className='hero-p'>Curated gifts to fulfill every Christmas wish</p>
        </div>
       );
     }
